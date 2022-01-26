b21959c6444a3cbbd6558f540775745541d3f263
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-26 07:55:46 -0600

    Guards and boolean added

--- exercism/elixir/Ejercicios.exs
+++ exercism/elixir/Ejercicios.exs
@@ -17,3 +17,47 @@ defmodule Lasagna do
   @spec alarm :: <<_::40>>
   def alarm, do: "Ding!"
 end
+
+# Booleanos
+
+defmodule Rules do
+  @spec eat_ghost?(boolean, boolean) :: boolean
+  def eat_ghost?(power_pellet_active, touching_ghost) do
+    power_pellet_active and touching_ghost
+  end
+
+  @spec score?(boolean, boolean) :: boolean
+  def score?(touching_power_pellet, touching_dot) do
+    touching_power_pellet or touching_dot
+  end
+
+  @spec lose?(boolean, boolean) :: boolean
+  def lose?(power_pellet_active, touching_ghost) do
+    is_posible_to_lose = not power_pellet_active
+    is_posible_to_lose and touching_ghost
+  end
+
+  @spec win?(boolean, boolean, boolean) :: boolean
+  def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
+    has_eaten_all_dots and not lose?(power_pellet_active, touching_ghost)
+  end
+end
+
+defmodule Rules2 do
+  @spec eat_ghost?(boolean, boolean) :: boolean
+  def eat_ghost?(true, true), do: true
+  def eat_ghost?(power_pellet_active, touching_ghost), do: false
+
+  @spec score?(boolean, boolean) :: boolean
+  def score?(false, false), do: false
+  def score?(touching_power_pellet, touching_dot), do: true
+
+  @spec lose?(boolean, boolean) :: boolean
+  def lose?(false, true), do: true
+  def lose?(power_pellet_active, touching_ghost), do: false
+
+  @spec win?(boolean, boolean, boolean) :: boolean
+  def win?(false, _, _), do: false
+  def win?(true, false, true), do: false
+  def win?(has_eaten_all_dots, power_pellet_active, touching_ghost), do: true
+end
