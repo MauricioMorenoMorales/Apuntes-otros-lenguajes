f53b4a70979ad56238e335bbcf7cbe54f00b4030
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-27 21:53:05 -0600

    Elixir lists

--- exercism/elixir/Ejercicios.exs
+++ exercism/elixir/Ejercicios.exs
@@ -61,3 +61,56 @@ defmodule Rules2 do
   def win?(true, false, true), do: false
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost), do: true
 end
+
+# ! Manipulacion de arrays
+
+defmodule LanguageList do
+  def new, do: []
+
+  def add(list, language), do: [language | list]
+
+  def remove(list) do
+    [_ | tail] = list
+    tail
+  end
+
+  def first(list) do
+    [head | _] = list
+    head
+  end
+
+  def count(list), do: length(list)
+
+  def exciting_list?(list) do
+    Enum.any?(list, fn element -> element == "Elixir" end)
+  end
+end
+
+defmodule LanguageList2 do
+  def new, do: []
+
+  def add(list, language), do: [language | list]
+
+  def remove([_ | tail]), do: tail
+
+  def first([head | _]), do: head
+
+  def count([]), do: 0
+  def count([_h | t]), do: 1 + count(t)
+
+  def exciting_list?(list), do: "Elixir" in list
+end
+
+defmodule LanguageList3 do
+  def new, do: []
+
+  def add(list, language), do: [language | list]
+
+  def remove(list), do: list |> tl
+
+  def first(list), do: list |> hd
+
+  def count(list), do: list |> length
+
+  def exciting_list?(list), do: "Elixir" in list
+end
