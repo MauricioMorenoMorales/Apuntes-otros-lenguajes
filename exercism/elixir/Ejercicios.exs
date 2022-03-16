b5cbb88ecbce20b0d4b20599a67e6be6be7b5fee
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-03-16 09:36:48 -0600

    Keyword lists

--- exercism/elixir/Ejercicios.exs
+++ exercism/elixir/Ejercicios.exs
@@ -1192,3 +1192,188 @@ defmodule RPG.CharacterSheet1 do
     |> IO.inspect(label: "Your character")
   end
 end
+
+#! Keyword list
+
+defmodule WineCellar do
+  def explain_colors, do:
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+
+  def filter(cellar, color, opts \\ []) do
+    year = Keyword.get(opts, :year)
+    country = Keyword.get(opts, :country)
+    case {year, country} do
+      {nil, nil} ->
+        cellar |> Keyword.get_values(color)
+      {_, nil} ->
+        cellar
+        |> Keyword.get_values(color)
+        |> filter_by_year(year)
+      {nil, _} ->
+        cellar
+        |> Keyword.get_values(color)
+        |> filter_by_country(country)
+      {_, _} ->
+        cellar
+        |> Keyword.get_values(color)
+        |> filter_by_country(country)
+        |> filter_by_year(year)
+    end
+  end
+
+  # The functions below do not need to be modified.
+
+  defp filter_by_year(wines, year)
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
+
+defmodule WineCellarCleverest do
+  def explain_colors, do:
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+
+  def filter(cellar, color, opts \\ []), do:
+    cellar
+    |> Keyword.get_values(color)
+    |> filter_by_year(opts[:year])
+    |> filter_by_country(opts[:country])
+
+  defp filter_by_year(wines, nil), do: wines
+  defp filter_by_country(wines, nil), do: wines
+
+  # The functions below do not need to be modified.
+
+  defp filter_by_year(wines, year)
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
+
+#! Ask for help
+defmodule WineCellar do
+  def explain_colors, do:
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+
+  def filter(cellar, color, opts \\ []), do:
+    cellar
+    |> Keyword.get_values(color)
+    |> then(&if year = opts[:year], do: filter_by_year(&1, year), else: &1)
+    |> then(&if country = opts[:country], do: filter_by_country(&1, country), else: &1)
+
+
+  # The functions below do not need to be modified.
+
+  defp filter_by_year(wines, year)
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
+
+defmodule WineCellar do
+  def explain_colors, do:
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+
+  def filter(cellar, color, opts \\ [])
+  
+  def filter(cellar, color, []), do: cellar |> Keyword.get_values(color)
+  def filter(cellar, color, filters) do
+    cellar = filter(cellar,color)
+    Enum.reduce(filters, cellar, fn
+      {:year, year}, cellar -> filter_by_year(cellar, year)
+      {:country, country}, cellar -> filter_by_country(cellar, country)
+    end)
+  end
+
+
+  # The functions below do not need to be modified.
+
+  defp filter_by_year(wines, year)
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
