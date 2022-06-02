bce12906db5732380affb87cde65430f2364db44
MauricioMorenoMorales <mauriciomorenomorales@protonmail.ch>
2022-06-01 20:00:24 -0500

    Testing

--- languages/Clojure.clj
+++ languages/Clojure.clj
@@ -17,7 +17,7 @@
 	(println "Success!")
 	"Abra cadabra")
 
-; or returns the first falsey value or the last truthy value
+; or returns the first truthy value or the last value
 
 (or false nil :large_I_mean_venti :why_cant_I_just_say_large); why_cant
 
@@ -25,7 +25,7 @@
 
 (or nil); nil
 
-; nad returns the first falsey value or the last truthy value
+; and returns the first falsey value or the last truthy value
 
 (and :free_wifi :hot_coffee); hot_coffee
 
@@ -99,3 +99,28 @@
 '(1 2 3 4 5)
 (nth '(:a :b :c) 0)
 (nth '(:a :b :c) 2)
+(list 1 "two" {3 4})
+
+; Add elements
+(conj '(1 2 3) 4)
+
+; Vectors vs lists, you shold use lists with macros and when you need to add elements to the beginning of the list
+
+;! Sets, filters repeated data
+#{"Kurt vonnegut" 20 :icicle}
+(hash-set "kurt vonnegut" 20 :icicle)
+
+(conj ${:a :ab} :b); #{:a :b}
+(set [3 3 3 4 4]); #{3 4}
+
+; Functions ===================================================================0
+
+; Example of functions
+(+ 1 2 3 4 5)
+(* 1 2 3 4 5)
+(first [1 2 3 4 5])
+
+; Functions that returns the addition function
+(or + -)
+((and (= 1 1) +) 1 2 3)
+((first [+ 0]) 1 2 3)
