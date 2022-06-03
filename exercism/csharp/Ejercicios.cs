835aada1cd1c2e8f15c130bb1218d238eb22a90c
MauricioMorenoMorales <mauriciomorenomorales@protonmail.ch>
2022-06-02 21:53:22 -0500

    C sharp dictionaries

--- exercism/csharp/Ejercicios.cs
+++ exercism/csharp/Ejercicios.cs
@@ -608,3 +608,162 @@ public static class Languages
     public static bool IsUnique(List<string> languages)
         => languages.Distinct().Count() == languages.Count();
 }
+
+
+public static class Languages
+{
+    public static List<string> NewList()
+        => new List<string>();
+
+    public static List<string> GetExistingLanguages()
+    {
+        List<string> response = new List<string>() {"C#", "Clojure", "Elm"};
+        return response;
+    }
+
+    public static List<string> AddLanguage(List<string> languages, string language)
+        => languages.Append(language).ToList();
+
+    public static int CountLanguages(List<string> languages)
+        => languages.Count();
+
+    public static bool HasLanguage(List<string> languages, string language)
+        => languages.Contains(language);
+
+    public static List<string> ReverseList(List<string> languages)
+        => languages.Reverse<string>().ToList();
+
+    public static bool IsExciting(List<string> languages)
+    {
+        if(languages.Count() == 0) return false;
+        else if(languages[0] == "C#") return true;
+        else if(languages[1] == "C#" && (languages.Count() == 2 || languages.Count == 3)) return true;
+        else return false;
+    }
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+        => languages.Where(element => element != language).ToList();
+
+    public static bool IsUnique(List<string> languages)
+        => languages.Distinct().Count() == languages.Count();
+}
+
+//! Dictionaries =============================================================================
+
+
+public static class DialingCodes
+{
+    public static Dictionary<int, string> GetEmptyDictionary()
+        => new Dictionary<int, string> {};
+
+    public static Dictionary<int, string> GetExistingDictionary()
+        => new Dictionary<int, string>{
+            [1] = "United States of America",
+            [55] = "Brazil",
+            [91] = "India"
+        };
+
+    public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
+    {
+        Dictionary<int, string> response = new Dictionary<int, string> {};
+        response[countryCode] = countryName;
+        return response;
+    }
+
+    public static Dictionary<int, string> AddCountryToExistingDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+    {
+        existingDictionary[countryCode] = countryName;
+        return existingDictionary;
+    }
+
+    public static string GetCountryNameFromDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode)
+        => existingDictionary.ContainsKey(countryCode)
+            ? existingDictionary[countryCode]
+            : string.Empty;
+
+    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
+        => existingDictionary.ContainsKey(countryCode);
+
+    public static Dictionary<int, string> UpdateDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+    {
+        if(existingDictionary.ContainsKey(countryCode))
+            existingDictionary[countryCode] = countryName;
+        return existingDictionary;
+    }
+
+    public static Dictionary<int, string> RemoveCountryFromDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode)
+    {
+        existingDictionary.Remove(countryCode);
+        return existingDictionary;
+    }
+
+    public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
+    {
+        string response = "";
+        foreach (KeyValuePair<int, string> element in existingDictionary)
+            if (element.Value.Length > response.Length)
+                response = element.Value;
+        return response;
+    }
+}
+
+// More functional version ===============================================================
+
+
+public static class DialingCodes
+{
+    public static Dictionary<int, string> GetEmptyDictionary()
+        => new();
+
+    public static Dictionary<int, string> GetExistingDictionary()
+        => new Dictionary<int, string>{
+            [1] = "United States of America",
+            [55] = "Brazil",
+            [91] = "India"
+        };
+
+    public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
+        => new() {
+            [countryCode] = countryName,
+        };
+
+    public static Dictionary<int, string> AddCountryToExistingDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+    {
+        existingDictionary.Add(countryCode, countryName);
+        return existingDictionary;
+    }
+
+    public static string GetCountryNameFromDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode)
+        => existingDictionary.ContainsKey(countryCode)
+            ? existingDictionary[countryCode]
+            : string.Empty;
+
+    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
+        => existingDictionary.ContainsKey(countryCode);
+
+    public static Dictionary<int, string> UpdateDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+    {
+        if(existingDictionary.ContainsKey(countryCode))
+            existingDictionary[countryCode] = countryName;
+        return existingDictionary;
+    }
+
+    public static Dictionary<int, string> RemoveCountryFromDictionary(
+        Dictionary<int, string> existingDictionary, int countryCode)
+    {
+        existingDictionary.Remove(countryCode);
+        return existingDictionary;
+    }
+
+    public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
+        => existingDictionary.Count == 0
+            ? string.Empty
+            : existingDictionary.Values.Max();
+}
