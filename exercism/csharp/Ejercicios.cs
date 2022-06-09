e91dcad5ccadc9d42b6c0272672f65608124041a
MauricioMorenoMorales <mauriciomorenomorales@protonmail.ch>
2022-06-08 21:12:21 -0500

    Bitewise

--- exercism/csharp/Ejercicios.cs
+++ exercism/csharp/Ejercicios.cs
@@ -915,3 +915,46 @@ public class Player
     public double GenerateSpellStrength()
         => random.NextDouble() * maxStrength;
 }
+
+//! Bitwise and flag enums ======================================================
+
+
+// TODO: define the 'AccountType' enum
+enum AccountType
+{
+    Guest,
+    User,
+    Moderator
+}
+
+// TODO: define the 'Permission' enum
+[Flags]
+enum Permission : byte
+{
+    Read = 1 << 0,
+    Write = 1 << 1,
+    Delete = 1 << 2,
+    All = Read | Write | Delete,
+    None = 0
+}
+
+
+static class Permissions
+{
+    public static Permission Default(AccountType accountType)
+    => accountType switch {
+        AccountType.Guest => Permission.Read,
+        AccountType.User => Permission.Read | Permission.Write,
+        AccountType.Moderator => Permission.All,
+        _ => Permission.None
+    };
+
+    public static Permission Grant(Permission current, Permission grant)
+    => current | grant;
+
+    public static Permission Revoke(Permission current, Permission revoke)
+    => current & ~revoke;
+
+    public static bool Check(Permission current, Permission check)
+    => current.HasFlag(check);
+}
