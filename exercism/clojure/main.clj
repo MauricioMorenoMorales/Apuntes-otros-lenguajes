aae3adfc00252b376c720f11080593b5c4685e9d
Mauricio Moreno Morales <64658355+MauricioMorenoMorales@users.noreply.github.com>
2023-03-09 08:27:52 -0600

    MArch 9

--- exercism/clojure/main.clj
+++ exercism/clojure/main.clj
@@ -290,3 +290,39 @@
       (if (= remaining nil)
         '(first face-cards))
       ))))
+
+;;? Second iteration
+
+
+(defn first-card
+  "Returns the first card from deck."
+  [[first _]]
+  first)
+
+(defn second-card
+  "Returns the second card from deck."
+  [[_ second]]
+  second)
+
+(defn swap-top-two-cards
+  "Returns the deck with first two items reversed."
+  [[first second & remaining]]
+  (->> [second first remaining]
+    (flatten)
+    (remove nil?)))
+
+(defn discard-top-card
+  "Returns a sequence containing the first card and
+   a sequence of the remaining cards in the deck."
+  [[first & remaining]]
+  [first remaining])
+
+(def face-cards
+  ["jack" "queen" "king"])
+
+(defn insert-face-cards
+  "Returns the deck with face cards between its head and tail."
+  ([[first & remaining]]
+  (->> [first face-cards remaining]
+    (flatten)
+    (remove nil?))))
